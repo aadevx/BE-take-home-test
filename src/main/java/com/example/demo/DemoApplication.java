de406020c706fb465814af51dcfdb5f2e8e5042c
arief ardiyansah <arief.ardiyansah@gmail.com>
2023-09-16 07:41:08 +0700

    set maven project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeTestApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeTestApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
